7761f658aec3dabe5cd16bd11db5a687aff62b64
Gabriel Nilsson <gabriel.nilsson@mindroad.se>
2025-02-14 13:08:22 +0100

    Support raspberry pi

--- boids.ino
+++ boids.ino
@@ -12,6 +12,7 @@
 #include "FastIMU.h"
 #include <Wire.h>
 
+#define ARDUINO_ON_NEW_BOARD 0
 #define IMU_ADDRESS 0x69    //Change to the address of the IMU
 //#define PERFORM_CALIBRATION //Comment to disable startup calibration
 BMI160 IMU;               //Change to the name of any supported IMU! 
@@ -308,29 +309,42 @@ void writeString(char* text) {
 
 
 void writeFloatString(float n) {
-  char buffer[128];
-  dtostrf(n, 4, 2, buffer);
-  int len = strlen(buffer);
-  Serial.print("Len:");
-  int desired_length = 6;
-  for (int i = 0; i < len - desired_length; ++i) {
-    writeString(" ");
-  }
-  writeString(buffer);
+    char buffer[128];
+#if BOARD_VENDORID == 0x2e8a && BOARD_PRODUCTID == 0x00c0
+    // If we're on raspberry pi pico
+    sprintf(buffer, "%.2f", n);
+#else
+    dtostrf(n, 4, 2, buffer);
+#endif
+    int len = strlen(buffer);
+    int desired_length = 6;
+    for (int i = 0; i < len - desired_length; ++i) {
+        writeString(" ");
+    }
+    writeString(buffer);
 }
 void setup() {
   Wire.begin();
   Wire.setClock(10000); //400khz clock
   Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
+  delay(1000);
+  //while (!Serial) {
+  //  ;
+  //}
 
-    pinMode(5,INPUT_PULLUP);
-    button1.attach(5);
+#if BOARD_VENDORID == 0x2e8a && BOARD_PRODUCTID == 0x00c0
+    // If we're on raspberry pi pico
+    unsigned button1_pin = 7;
+    unsigned button2_pin = 8;
+#else
+    unsigned button1_pin = 5;
+    unsigned button2_pin = 4;
+#endif
+    pinMode(button1_pin,INPUT_PULLUP);
+    button1.attach(button1_pin);
     button1.interval(10);// interval in ms
-    pinMode(4,INPUT_PULLUP);
-    button2.attach(4);
+    pinMode(button2_pin,INPUT_PULLUP);
+    button2.attach(button2_pin);
     button2.interval(10); // interval in ms
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
@@ -405,8 +419,15 @@ void loop() {
         display.setCursor(0, 0);
 
         float factor = 2.5;
-        int16_t x = CIRCLE_RADIUS + ((accelData.accelY*factor + 1) / 2) * (SCREEN_WIDTH - CIRCLE_RADIUS);
-        int16_t y = CIRCLE_RADIUS + ((accelData.accelX*factor + 1) / 2) * (SCREEN_HEIGHT - CIRCLE_RADIUS);
+#if (BOARD_VENDORID == 0x2e8a && BOARD_PRODUCTID == 0x00c0) || ARDUINO_ON_NEW_BOARD != 0
+        float rawX = -accelData.accelX;
+        float rawY = accelData.accelY;
+#else
+        float rawX = accelData.accelY;
+        float rawY = accelData.accelX;
+#endif
+        int16_t x = CIRCLE_RADIUS + ((rawX*factor + 1) / 2) * (SCREEN_WIDTH - CIRCLE_RADIUS);
+        int16_t y = CIRCLE_RADIUS + ((rawY*factor + 1) / 2) * (SCREEN_HEIGHT - CIRCLE_RADIUS);
         vec3_t predator_location = vec3_t(x, y);
 //  writeFloatString(accelData.accelX);
 //  writeString(", ");
